832efe6e301acbe5470853af8c2b9266a05f5f96
toptenov <toptenov@gmail.com>
2020-10-01 10:02:35 +0300

    Внес правки, согласно комментариям ревьюеров

--- homeworks/lesson3_6/lesson3_6_step9/conftest.py
+++ homeworks/lesson3_6/lesson3_6_step9/conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-	parser.addoption('--language', action='store', default="en", help="Choose language: ar, ca, cs, da, de, en, el, es, fi, fr, it, ko, nl, pl, pt, pt-br, ro, ru, sk, uk, zh-cn")
+	parser.addoption('--language', action='store', default="en", help="Choose language")
 
 @pytest.fixture(scope="function")
 def browser(request):
@@ -15,8 +15,3 @@ def browser(request):
 	yield browser
 	print("\nquit browser..")
 	browser.quit()
-
-@pytest.fixture()
-def language(request):
-	lang = request.config.getoption("language")
-	return lang
