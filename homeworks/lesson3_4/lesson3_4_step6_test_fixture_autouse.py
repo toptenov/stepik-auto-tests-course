5bfb7d5b70853e9a249cef702a2ca410887cfbb4
toptenov <toptenov@gmail.com>
2020-10-06 20:49:59 +0300

    Я забыл что включает в себя этот коммит

--- homeworks/lesson3_4/lesson3_4_step6_test_fixture_autouse.py
+++ homeworks/lesson3_4/lesson3_4_step6_test_fixture_autouse.py
@@ -22,9 +22,8 @@ class TestMainPage1():
     def test_guest_should_see_login_link(self, browser):
         # не передаём как параметр фикстуру prepare_data, но она все равно выполняется
         browser.get(link)
-        browser.find_element_by_css_selector("#login_link")
+        login_link = browser.find_element_by_css_selector("#login_link1")
 
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector(".basket-mini .btn-group > a")
-        
